4312e3c5c9afc09b4d1b94ebdc8999e41c1b0cd5
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-03-22 15:08:20 +0530

    login page ui done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,33 +1,77 @@
-import React, { useContext,useEffect,useState } from 'react'
-import { useHistory } from 'react-router'
-import FirebaseContext from '../context/firebase'
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory,Link } from "react-router-dom";
 
-const  Login = () => {
-    const history = useHistory()
-    const {firebase} = useContext(FirebaseContext)
+import FirebaseContext from "../context/firebase";
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+const Login = () => {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [error, setError] = useState('')
-    const isInvalid = password ==="" || email === "" 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = () => {}
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || email === "";
 
-    useEffect(()=>{
-        document.title="Login | Instagram"
-    },[])
+  const handleLogin = () => {};
 
-    return (
-        <div className="mx-auto container flex max-w-screen-md items-center h-screen" >
-         <div className=" flex w-3/5 ">
-             <img src="/images/iphone-with-profile.jpg" alt="iphone with profile" className="max-w-xs" />
-        </div>
-        <div className="flex flex-col w-2/5">
-            <p>This is the form</p>
-        </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    document.title = "Login | Instagram";
 
-export default Login
+  }, []);
+
+  return (
+    <div className="mx-auto container flex  max-w-screen-md items-center h-screen">
+      <div className=" flex w-3/5 ">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iphone with profile"
+          className="max-w-xs"
+        />
+      </div>
+      <div className="flex flex-col w-2/5 ">
+          <div className="bg-white p-4 items-center mb-4 border border-gray-primary">
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="" />
+        </h1>
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form method="POST" onSubmit={handleLogin}>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            aria-label="Enter your email address"
+            placeholder="Email Address"
+            className="text-sm text-grey-base w-full py-5 px-4 mr-3 h-2 border border-gray-primary rounded mb-2"
+          />
+
+           <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            aria-label="Enter your password"
+            placeholder="Password"
+            className="text-sm text-grey-base w-full py-5 px-4 mr-3 h-2 border border-gray-primary rounded mb-2"
+          />
+
+          <button
+          disabled={isInvalid}
+          type="submit"
+          className={
+              `bg-blue-500 w-full text-white rounded h-8 font-bold ${isInvalid && "opacity-50"}`}
+          >
+            Log In 
+        </button>
+        </form>
+      </div>
+      <div className="flex justify-center flex-col items-center bg-white w-full p-4 border border-gray-primary">
+          <p className="text-sm">Don't have an accound yet? 
+          <Link to="/signup" className="font-bold text-blue-medium "> Sign up</Link> </p>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
